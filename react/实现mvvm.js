4adb1c9fc69e7dd7c49fe75a3dc8201d362ec4f0
weizy <1226393396@qq.com>
2020-07-29 17:24:40 +0800

    feat

--- react/实现mvvm.js
+++ react/实现mvvm.js
@@ -6,27 +6,115 @@
  * 反之，亦然
  * vue采用办法数据劫持，对数据（Model）进行劫持，当数据发生变动时，数据会触发劫持时绑定的方法，对视图进行更新
  */
-var data = {name: 'kindeng',obj:{a:1}};
-observe(data);
-// data.name = 'wzy'; // 哈哈哈，监听到值变化了 kindeng --> dmq
-// console.log(data.name) 
 
-function observe(data) {
-  const keyList = Object.keys(data)
-  keyList.map(item=>{
-    let val = data[item]
-    console.log(val)
-    // observe(val)
-    Object.defineProperty(data, item, {
-      enumerable: true,
-      configurable: true,
-      get() {
-        return value
-      },
-      set(value) {
-        val = value
-        console.log('set成功：'+value);
+class Mvvm {
+  constructor(options) {
+      const { el, data, methods } = options;
+      this.methods = methods;
+      this.target = null;
+      // 初始化dispatcher
+      this.observe(this, data);
+      // 初始化watcher
+      this.compile(document.getElementById(el));
+  }
+
+  observe(root, data) {
+      for (const key in data) {
+          this.defineReactive(root, key, data[key]);
       }
-    });
-  })
+  }
+
+  defineReactive(root, key, value) {
+      if (typeof value == 'object') {
+          return this.observe(value, value);
+      }
+      const dep = new Dispatcher();
+      Object.defineProperty(root, key, {
+          set(newValue) {
+              if (value == newValue) return;
+              value = newValue;
+              // 发布
+              dep.notify(newValue);
+          },
+          get() {
+              // 订阅
+              console.log(this.target)
+              dep.add(this.target);
+              return value;
+          }
+      });
+  }
+
+  compile(dom) {
+      const nodes = dom.childNodes;
+      for (const node of nodes) {
+          // 元素节点
+          if (node.nodeType == 1) {
+              const attrs = node.attributes;
+              for (const attr of attrs) {
+                  if (attr.name == 'v-model') {
+                      const name = attr.value;
+                      node.addEventListener('input', e => {
+                          this[name] = e.target.value;
+                      });
+                      this.target = new Watcher(node, 'input');
+                      this[name];
+                  }
+                  if (attr.name == '@click') {
+                      const name = attr.value;
+                      node.addEventListener('click', this.methods[name].bind(this));
+                  }
+              }
+          }
+          // text节点
+          if (node.nodeType == 3) {
+              const reg = /\{\{(.*)\}\}/;
+              const match = node.nodeValue.match(reg);
+              if (match) {
+                  const name = match[1].trim();
+                  this.target = new Watcher(node, 'text');
+                  this[name];
+              }
+          }
+      }
+  }
 }
+
+class Dispatcher {  // 发布订阅，相当于eventHub
+  constructor() {
+      this.watchers = [];
+  }
+  add(watcher) {
+      this.watchers.push(watcher);
+  }
+  notify(value) {
+      this.watchers.forEach(watcher => watcher.update(value));
+  }
+}
+
+class Watcher {
+  constructor(node, type) {
+      this.node = node;
+      this.type = type;
+  }
+  update(value) {
+      if (this.type == 'input') {
+          this.node.value = value;
+      }
+      if (this.type == 'text') {
+          this.node.nodeValue = value;
+      }
+  }
+}
+
+var vm = new Mvvm({
+  el: 'app',
+  data: {
+      text: 'hello world'
+  },
+  methods: {
+      reset() {
+          this.text = '';
+      },
+  },
+});
---
+++ react/实现redux.js
@@ -0,0 +1,84 @@
+const appState = {
+  title: {
+    text: 'React.js 小书',
+    color: 'red',
+  },
+  content: {
+    text: 'React.js 小书内容',
+    color: 'blue'
+  }
+}
+
+function createStore(reducer){
+  let state = null
+  const listeners = [] // dispatch之后触发的函数
+  const subscribe = (listener) => listeners.push(listener)
+  const getState = ()=>state
+  const dispatch = (action)=>{
+    state = reducer(state,action)
+    listeners.forEach(item =>item())
+  }
+  dispatch({}) // 初始化 state
+  return {subscribe,getState,dispatch}
+}
+
+function reducer (state, action) {
+  // 初始化state
+  if (!state) return {
+    themeName: 'Red Theme',
+    themeColor: 'red'
+  }
+  switch (action.type) {
+    case 'UPDATE_TITLE_TEXT':
+      return { // 构建新的对象并且返回
+        ...state,
+        title: {
+          ...state.title,
+          text: action.text
+        }
+      }
+    case 'UPDATE_TITLE_COLOR':
+      return { // 构建新的对象并且返回
+        ...state,
+        title: {
+          ...state.title,
+          color: action.color
+        }
+      }
+    default:
+      return state // 没有修改，返回原来的对象
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    themeColor: state.themeColor
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSwitchColor: (color) => {
+      dispatch({ type: 'CHANGE_COLOR', themeColor: color })
+    }
+  }
+}
+
+// react-redux 把store放到Provider组件的context里面
+// connect(传入的state,传入的action)返回一个函数，这个函数是高阶组件，参数是要包裹的组件
+export const connect = (mapStateToProps,mapDispatchToProps) => (WrappedComponent) => {
+  class Connect extends Component {
+    static contextTypes = {
+      store: PropTypes.object
+    }
+
+    render () {
+      const { store } = this.context
+      let stateProps = mapStateToProps(store.getState())
+      let dispatchProps = mapDispatchToProps(store.dispatch, this.props)
+      return <WrappedComponent {...stateProps} {...dispatchProps} />
+    }
+  }
+
+  return Connect
+}
