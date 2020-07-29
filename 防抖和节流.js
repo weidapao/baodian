9f140752b7eee649d446a437d13e73e5c417bd31
weizy <1226393396@qq.com>
2020-07-29 18:01:35 +0800

    feat

---
+++ 防抖和节流.js
@@ -0,0 +1,66 @@
+function debounce(callback,wait){
+  let timer = null;
+  return ()=>{
+    console.log(this)
+    if(timer){
+      clearTimeout(timer)  
+    }
+    timer = setTimeout(callback,wait)
+  }
+}
+
+// 时间戳版
+function throttle(func, wait) {
+  var previous = 0;
+  return function() {
+      let now = Date.now();
+      let context = this;
+      let args = arguments;
+      if (now - previous > wait) {
+          func.apply(context, args);
+          previous = now;
+      }
+  }
+}
+
+// 时间到之后把time设置为null，然后才能执行下次
+function throttle2(func, wait) {
+  let timeout;
+  return function() {
+      let context = this;
+      let args = arguments;
+      if (!timeout) {
+          timeout = setTimeout(() => {
+              timeout = null;
+              func.apply(context, args)
+          }, wait)
+      }
+
+  }
+}
+
+var throttle3 = function(func, delay) {
+  var timer = null;
+  var startTime = Date.now();
+  return function() {
+          var curTime = Date.now();
+          var remaining = delay - (curTime - startTime);
+          var context = this;
+          var args = arguments;
+          clearTimeout(timer);
+           if (remaining <= 0) {
+                 func.apply(context, args);
+                 startTime = Date.now();
+           } else {
+                 timer = setTimeout(func, remaining);
+           }
+   }
+}
+
+var a = debounce(()=>{console.log(3)},3000)
+a()
+a()
+a()
+a()
+a()
+a()
